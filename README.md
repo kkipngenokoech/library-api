5a27deda66a108d024cce23e7b3c08807a1c391f
kip <kkipngenokoech22@gmail.com>
2023-04-22 22:13:11 +0300

    i want to install django famrework

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # library-api
+
 here i will build out a basic Library website with traditional Django and then extend it into a web API with Django REST Framework.
+
+## prerequiste
+
+one has to build the traditional django application first with the MVT, and as for now we hava already built the library MVT
+
+## library api
+
+Our Library website currently consists of a single page that displays all books in the database. To
+transform it into a web API we will install Django REST Framework and create a new URL that
+acts an API endpoint outputting all available books
+
+a web API does not output a traditional webpage with HTML, CSS, and JavaScript. Instead, it is just pure data
+( often in the JSON format) and accompanying HTTP verbs that specify what user actions are
+allowed. In this instance, an API user can only read the content, they are not able to update it in
+any way though we will learn how to do that in future chapters.
+
+## django rest framework
+
+    
--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
 # Create your tests here.
+class BookTest(TestCase):
+    @classmethod
+    def SetUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = 'The Lord of the Rings',
+            subtitle = 'the lord of rings subtitles',
+            author = 'J. R. R. Tolkien',
+            isbn = '0-553-21311-3',
+        )
+
+    def testBookInstance(self):
+        self.assertEqual(self.book.title, 'The Lord of the Rings')
+        self.assertEqual(self.book.subtitle, 'the lord of rings subtitles')
+        self.assertEqual(self.book.author, 'J. R. R. Tolkien')
+        self.assertEqual(self.book.isbn, '0-553-21311-3')
+
+    def testBookListView(self):
+        response = self.client.get(reverse('list'))
+        self.assertEqual(response.status_code, 200)
+        # self.assertContains(response, 'lord of rings ')
+        self.assertTemplateUsed(response, "books/book_list.html")
