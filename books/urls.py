d29f38b2ba1a5e0e754bb4451b91d9cf0ebee149
kip <kkipngenokoech22@gmail.com>
2023-04-22 21:27:18 +0300

    book title details

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("",views.HomePage, name="home")
+    path("",views.HomePage, name="home"),
+    path("list",views.BookListView.as_view(), name="list")
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from.models import Book
 
 # Create your views here.
 def HomePage(request):
     return HttpResponse("Welcome to multiverse")
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = "books"
+    # template_name = 'book_list.html'
